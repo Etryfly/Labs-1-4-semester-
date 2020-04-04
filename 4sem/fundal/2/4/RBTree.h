b99cc8a65c37cee4350d6c6dbf6d0777c3d88e6e
etryfly <etrymax@gmail.com>
2020-04-04 16:36:59 +0300

    2.2 fix

---
+++ 4sem/fundal/2/4/RBTree.h
@@ -0,0 +1,22 @@
+//
+// Created by etryfly on 28.03.2020.
+//
+
+#ifndef INC_2_RBTREE_H
+#define INC_2_RBTREE_H
+
+
+
+template <class T>
+class RBTree {
+    RBTree * right;
+    RBTree * left;
+    RBTree * parent;
+    T* data;
+    bool color;
+
+};
+
+
+
+#endif //INC_2_RBTREE_H
---
+++ 4sem/fundal/2/4/main.cpp
@@ -0,0 +1,4 @@
+//
+// Created by etryfly on 03.04.2020.
+//
+
