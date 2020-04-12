40cbc3aba4281cb132dd44894432bf242ab0fbb4
etryfly <etrymax@gmail.com>
2020-04-12 21:03:15 +0300

    1.4

--- 4sem/fundal/2/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ 4sem/fundal/2/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -51,7 +51,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/test.dir/DependInfo.cmake"
   "CMakeFiles/4.dir/DependInfo.cmake"
   "CMakeFiles/1.dir/DependInfo.cmake"
   "CMakeFiles/2.dir/DependInfo.cmake"
